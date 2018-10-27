3b2f4d4ac20b1585c984234f32011eb27ffb17c7
Raoul Bickmann <raoul.bickmann@googlemail.com>
2018-10-27 18:15:28 +0200

    example program

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
 
+const int triggerPin = 7;
+const int echoPin = 8;    
+
+long duration;
+long distance;
+
 void setup() {
-    // put your setup code here, to run once:
+    pinMode(triggerPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    Serial.begin(9600); 
+}
+
+void printDistance() {
+    digitalWrite(triggerPin, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin, LOW);
+
+    duration = pulseIn(echoPin, HIGH);
+
+    distance = duration * 0.034/2;
+
+    Serial.print("Distance: ");
+    Serial.println(distance);  
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    delay(100);
+    printDistance();
 }
