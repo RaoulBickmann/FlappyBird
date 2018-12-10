0a31aba56a261642392d84068e8b33b805fb9753
RaoulBickmann <raoul.bickmann@googlemail.com>
2018-12-10 17:07:56 +0100

    v1 of timing with millis

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
+#include <time.h>
 
 const int triggerPin = 7;
 const int echoPin = 8;    
 
 long duration;
 float distance;
+
+long lastMillis;
 
 
 void setup() {
@@ -15,20 +18,25 @@ void setup() {
 }
 
 void printDuration() {
-    digitalWrite(triggerPin, LOW);
-    delayMicroseconds(2);
 
-    digitalWrite(triggerPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(triggerPin, LOW);
 
-    duration = pulseIn(echoPin, HIGH);
+    if(millis() - lastMillis >= 500) {
+        digitalWrite(triggerPin, LOW);
+        delayMicroseconds(2);
 
-    distance = duration * 0.034/2;
+        digitalWrite(triggerPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(triggerPin, LOW);
+        
+        duration = pulseIn(echoPin, HIGH, 3333);
+        distance = duration * 0.034/2;
 
-    Serial.print(millis());
-    Serial.print(", ");
-    Serial.println(duration);
+        Serial.print(millis());
+        Serial.print(", ");
+        Serial.println(duration);
+        lastMillis = millis();
+
+    }
 }
 
 void loop() {
