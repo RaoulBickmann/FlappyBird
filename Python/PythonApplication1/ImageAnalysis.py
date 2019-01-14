3d18e49daef5f4965dd6d8606756add13cb36b6e
RaoulBickmann <raoul.bickmann@googlemail.com>
2019-01-14 23:12:48 +0100

    added data for videos without the videos

---
+++ Python/PythonApplication1/ImageAnalysis.py
@@ -0,0 +1,107 @@
+import cv2
+import sys
+import numpy as np
+ 
+def analyseImage():  
+    # Set up tracker.
+    # Instead of MIL, you can also use
+ 
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    tracker_type = tracker_types[7]
+ 
+    #if int(minor_ver) < 3:
+    #    tracker = cv2.Tracker_create(tracker_type)
+    #else:
+    if tracker_type == 'BOOSTING':
+        tracker = cv2.TrackerBoosting_create()
+    if tracker_type == 'MIL':
+        tracker = cv2.TrackerMIL_create()
+    if tracker_type == 'KCF':
+        tracker = cv2.TrackerKCF_create()
+    if tracker_type == 'TLD':
+        tracker = cv2.TrackerTLD_create()
+    if tracker_type == 'MEDIANFLOW':
+        tracker = cv2.TrackerMedianFlow_create()
+    if tracker_type == 'GOTURN':
+        tracker = cv2.TrackerGOTURN_create()
+    if tracker_type == 'MOSSE':
+        tracker = cv2.TrackerMOSSE_create()
+    if tracker_type == "CSRT":
+        tracker = cv2.TrackerCSRT_create()
+        atracker = cv2.TrackerCSRT_create()
+ 
+    # Read video
+    video = cv2.VideoCapture("movie_movie2.mov")
+ 
+    # Exit if video not opened.
+    if not video.isOpened():
+        print ("Could not open video")
+        sys.exit()
+ 
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print ('Cannot read video file')
+        sys.exit()
+     
+    # Define an initial bounding box
+    bbox = (287, 23, 86, 320)
+    abox = (287, 23, 86, 320)
+ 
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI(frame, False)
+    abox = cv2.selectROI(frame, False)
+ 
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
+    ok = atracker.init(frame, abox)
+    counter = 0;
+ 
+    while True:
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
+            break
+         
+        # Start timer
+        timer = cv2.getTickCount()
+ 
+        # Update tracker
+        ok, bbox = tracker.update(frame)
+        ok, abox = atracker.update(frame)
+ 
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+ 
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))    
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+            
+            a1 = (int(abox[0]), int(abox[1]))
+            a2 = (int(abox[0] + abox[2]), int(abox[1] + abox[3]))
+            cv2.rectangle(frame, a1, a2, (0,0,255), 2, 1)
+
+            m1 = int((p1[0] + p2[0]) / 2)
+            m2 = int((a1[0] + a2[0]) / 2)
+
+            print(str(counter) + ": " + str(m1 - m2))
+            counter = counter + 1
+        else :
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+ 
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+     
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+ 
+        # Display result
+        cv2.imshow("Tracking", frame)
+ 
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27 : break
--- Python/PythonApplication1/PythonApplication1.py
+++ Python/PythonApplication1/PythonApplication1.py
@@ -6,28 +6,41 @@ import sympy as sp
 import UtilityFunctions as uf
 import KalmanFilter as kalman
 import Simulation as sim
+import ImageAnalysis as ima
+#data = uf.ReadData('14sec_backandforth', False)
 
-data = uf.ReadData('14sec_backandforth', False)
+#y = np.array(data.cm)
+#N = len(data.cm)
+#Ts = 0.02
+#t = data.time
 
-y = np.array(data.cm)
-N = len(data.cm)
-Ts = 0.02
-t = data.time
+##s_groundtruth = np.ones(N);
+##s_groundtruth *= 20;
 
-#s_groundtruth = np.ones(N);
-#s_groundtruth *= 20;
+##v = np.zeros(N);
+##a = np.zeros(N);
 
-#v = np.zeros(N);
-#a = np.zeros(N);
+##y = uf.preFilter(y)
+##y = uf.preFilter2(y)
+#print(np.std(y))
+#x, P = kalman.filter(y, N, Ts)
 
-#y = uf.preFilter(y)
-#y = uf.preFilter2(y)
-print(np.std(y))
-x, P = kalman.filter(y, N, Ts)
+##uf.plot_filterresult(t, y, s_groundtruth, v, a, x, P)
+##uf.plot_filteronly(t, y, x, P)
 
-#uf.plot_filterresult(t, y, s_groundtruth, v, a, x, P)
-#uf.plot_filteronly(t, y, x, P)
-
-sim.runSimulation()
+#sim.runSimulation()
 
 
+#ima.analyseImage();
+
+
+data = pd.read_csv('camera_tracking.csv')
+
+millis = np.arange(20, 22960, 20)
+counter = 0
+
+for n in range(len(millis)):
+    if((counter) * 33.333 < millis[n]):
+        counter = counter + 1
+
+    print(str(data.frame[counter]) + " " + str(millis[n]) + " " + str(data.pixel[counter]/53 + 20))
