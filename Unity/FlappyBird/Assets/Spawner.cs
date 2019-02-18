fc155ea74b9a998a3f5cedd311260272bc3f1520
RaoulBickmann <raoul.bickmann@googlemail.com>
2019-02-18 14:42:46 +0100

    demo in unity

---
+++ Unity/FlappyBird/Assets/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float spawnRate = 3f;
+    public GameObject obstacle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnObstacle", 0, spawnRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnObstacle()
+    {
+        float y = Random.Range(0, 25);
+        Vector3 position = new Vector3(transform.position.x, 30 - y, transform.position.z);
+        GameObject hindernis = Instantiate(obstacle, position, Quaternion.identity);
+        Destroy(hindernis, 5);
+    }
+}
